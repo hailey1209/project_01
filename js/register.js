91718e237ca58c9e1227b04a48a359d443f62d74
Hailey <ghkdldua50@gmail.com>
2023-08-16 16:22:09 +0900

    회원가입 페이지 구현

---
+++ js/register.js
@@ -0,0 +1,33 @@
+const scroller = new Scroller(false)
+
+window.onload = () => {
+    scroller.setScrollPosition({top:0, behavior:'smooth'})
+  
+    //테마변경 다크
+    const mode = document.querySelector('.mode')
+    const header = document.querySelector('header')
+    const hambtn = document.querySelector('header .navbar .ham-btn')
+    const icons = document.querySelectorAll('header .navbar .mode .material-symbols-outlined')
+    const scrollBtn = document.querySelectorAll('main .main-scroll .scroll-container .btns button')
+    const scrollItems = document.querySelectorAll('main .main-scroll .scroll-container .item-container .item')
+  
+    mode.addEventListener('click', (e) => {
+      document.body.classList.toggle('dark')
+      header.classList.toggle('dark')
+      hambtn.classList.toggle('dark')
+  
+      for(let item of scrollItems){
+        item.classList.toggle('dark')
+      }
+      for (let btn of scrollBtn){
+        btn.classList.toggle('dark')
+      }
+  
+      for(let icon of icons){  //모드 버튼의 display설정 변경
+        icon.classList.contains('show') ? 
+        icon.classList.remove('show')
+        : icon.classList.add('show')
+      }
+    })
+  
+  }
